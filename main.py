86602cd81aff43a80507dd250ea37c06ec41d1f0
Nanorneirbo <47759615+Nanorneirbo@users.noreply.github.com>
2019-11-10 20:48:41 +0000

    Added a print function with a couple of arguments

--- main.py
+++ main.py
@@ -13,7 +13,13 @@ def break_line():
     print("-------------------------------------------------------------------")
 
 
+def arguments_function(first, second):
+    print("this is the first one - " + first)
+    print("this is the second - " + second)
+
 break_line()
 my_function()
+break_line()
+arguments_function("Number 1", "number 2")
 
 
