f10603f2b0d432dcc300c0e2b752d21e4a2a8344
Nanorneirbo <47759615+Nanorneirbo@users.noreply.github.com>
2019-11-10 20:45:03 +0000

    A couple of printed functions

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+# the main will be for concepts etc.
+
+print("skipping to functions")
+
+
+def my_function():
+    print("this is a function")
+
+
+def break_line():
+    print("-------------------------------------------------------------------")
+
+
+break_line()
+my_function()
+
+
