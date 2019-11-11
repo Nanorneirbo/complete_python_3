ab1863287cd19f6274ba83f0b7fb2ddb7f1863ca
Nanorneirbo <47759615+Nanorneirbo@users.noreply.github.com>
2019-11-11 18:54:53 +0000

    attack.py

---
+++ Early Stuff/advanced.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+
+def my_function(one, two,
+                three, four,
+                five, six):
+    print("Hello World")
+
+
+def second_function():
+    print("Second function")
+
+
+my_list = [1, 2,
+           3, 4,
+           5, 6]
+
+# Breaking out of while loop
