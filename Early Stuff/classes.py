65a12989de64017ef7eb660b6417ee9168656572
Nanorneirbo <47759615+Nanorneirbo@users.noreply.github.com>
2019-11-11 19:17:22 +0000

    attack - set variables

--- Early Stuff/classes.py
+++ Early Stuff/classes.py
@@ -2,11 +2,24 @@ import random
 
 
 class Enemy:
-    atk_l = 60
-    atk_h = 80
+    hp = 200
+
+    def __init__(self, atk_l, atk_h):
+        self.atk_l = atk_l
+        self.atk_h = atk_h
 
     def get_atk(self):
         print(self.atk_l)
 
-enemy1 = Enemy()
+    def get_hp(self):
+        print(("Hp is", self.hp))
+
+
+enemy1 = Enemy(40, 49)
 enemy1.get_atk()
+enemy1.get_hp()
+
+enemy2 = Enemy(75, 90)
+enemy2.get_atk()
+enemy2.get_hp()
+
