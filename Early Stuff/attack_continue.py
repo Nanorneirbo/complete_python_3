55d9f4d1cf187cfc8077f78fefa317fe3ac1b4a0
Nanorneirbo <47759615+Nanorneirbo@users.noreply.github.com>
2019-11-11 19:05:53 +0000

    continue statement

---
+++ Early Stuff/attack_continue.py
@@ -0,0 +1,22 @@
+import random
+
+
+player_hp = 260
+enemy_atk_low = 60
+enemy_atk_high = 80
+
+while player_hp > 0:
+    damage = random.randrange(enemy_atk_low, enemy_atk_high)
+    player_hp = player_hp-damage
+
+    if player_hp <= 30:
+        player_hp = 30
+
+    print("Enemy strikes for", damage, "points of damage. Current HP is", player_hp)
+
+    if player_hp > 30:
+        continue
+
+    print("You have low health you have been teleported to nearest inn")
+    break
+
